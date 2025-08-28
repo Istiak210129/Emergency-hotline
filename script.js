ec0c11bad3a9a309477eb1e59402165f64132838
Istiak <istiakuddin929@gmail.com>
2025-08-28 16:19:27 +0600

    call history added

--- script.js
+++ script.js
@@ -13,23 +13,75 @@ for(let heart of hearts)
 
 //  call button handler
 
+//function for getelementid
+ function getElement(id){
+    let element = document.getElementById(id);
+    return element;
+ }
+
+ // function for alert message
+function getalert(name,number){
+    alert(`${name}  ${" "} ${number}`)
+}
+
+
+
+
+
 let calls = document.getElementsByClassName("callBtn");
 
 for(let call of calls){
     call.addEventListener("click",function(){
 
+    // let services = document.getElementsByClassName("serviceName")
+    
+   
+    // console.log(serviceName,serviceNumber);
+    let serviceName = call.parentNode.parentNode.childNodes[5].innerText;
+    let serviceNumber = call.parentNode.parentNode.childNodes[7].innerText;
+    // console.log(serviceName,serviceNumber);
 
-        
-        
+    getalert(serviceName,serviceNumber);
+
         let coin = document.getElementById("coinBtn");
-        
-        coin.innerText = parseInt(coin.innerText) - 20;
-
-        if(coin.innerText < 20)
+          if(coin.innerText < 20  )
         {
             alert("you have not Sufficient balance");
             return;
         }
 
+        coin.innerText =  Number(coin.innerText) - 20;
+
+      
+
+            let data = {
+                name:serviceName,
+                num: serviceNumber,
+                time:new Date().toLocaleTimeString()
+
+            }
+            // historyData.push(data);
+
+            let historyCard = document.getElementById("history-section");
+
+          
+                  let div = document.createElement("div");
+            div.innerHTML = `<div id="history-contain" class="history flex  p-2 items-center justify-between my-4 rounded-lg bg-[#F2F2F2]">
+                    <div class="content">
+                    <h1 id="serviceh" class="font-bold">${data.name}</h1>
+                    <p id="servicehno" class="text-gray-600">${data.num}</p>
+                    </div>
+                    <div id="local-time" class="time">
+                        <h2>${data.time}</h2>
+                    </div>
+                </div>`;
+
+            historyCard.appendChild(div);
+
+           
+
+          
     })
+    
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,2 @@
-// heart icon handler
 
-document.getElementById("heartbtn")
-.addEventListener("click",function(){
-    let count = document.getElementById("")
-})
+
